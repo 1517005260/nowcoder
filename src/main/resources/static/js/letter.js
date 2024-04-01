59c3624219b038ece323f931045008bdfe989273
GLK <1517005260@qq.com>
2024-04-01 21:17:30 +0800

    changed frontend resources to origin verion

--- src/main/resources/static/js/letter.js
+++ src/main/resources/static/js/letter.js
@@ -5,27 +5,10 @@ $(function(){
 
 function send_letter() {
 	$("#sendModal").modal("hide");
-
-	var toName = $("#recipient-name").val();
-	var content = $("#message-text").val();
-	$.post(
-		CONTEXT_PATH + "/letter/send",
-		{"toName":toName,"content":content},
-		function(data) {
-			data = $.parseJSON(data);
-			if(data.code == 0) {
-				$("#hintBody").text("发送成功!");
-			} else {
-				$("#hintBody").text(data.msg);
-			}
-
-			$("#hintModal").modal("show");
-			setTimeout(function(){
-				$("#hintModal").modal("hide");
-				location.reload();
-			}, 2000);
-		}
-	);
+	$("#hintModal").modal("show");
+	setTimeout(function(){
+		$("#hintModal").modal("hide");
+	}, 2000);
 }
 
 function delete_msg() {
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -6,33 +6,9 @@ function follow() {
 	var btn = this;
 	if($(btn).hasClass("btn-info")) {
 		// 关注TA
-		$.post(
-			CONTEXT_PATH + "/follow",
-			{"entityType":3,"entityId":$(btn).prev().val()},
-			function(data) {
-				data = $.parseJSON(data);
-				if(data.code == 0) {
-					window.location.reload();
-				} else {
-					alert(data.msg);
-				}
-			}
-		);
-		// $(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
+		$(btn).text("已关注").removeClass("btn-info").addClass("btn-secondary");
 	} else {
 		// 取消关注
-		$.post(
-			CONTEXT_PATH + "/unfollow",
-			{"entityType":3,"entityId":$(btn).prev().val()},
-			function(data) {
-				data = $.parseJSON(data);
-				if(data.code == 0) {
-					window.location.reload();
-				} else {
-					alert(data.msg);
-				}
-			}
-		);
-		//$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
+		$(btn).text("关注TA").removeClass("btn-secondary").addClass("btn-info");
 	}
 }
